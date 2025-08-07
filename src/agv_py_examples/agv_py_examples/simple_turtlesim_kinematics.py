bcba3545a0223638eca2d36861dc79e83610af99
dionisioraditya <dio.prasmada@gmail.com>
2025-08-07 18:03:24 +0700

    update Rotation Matrix

--- src/agv_py_examples/agv_py_examples/simple_turtlesim_kinematics.py
+++ src/agv_py_examples/agv_py_examples/simple_turtlesim_kinematics.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
-
+import math
 
 class SimpleTurtlesimKinematics(Node):
     def __init__(self):
@@ -23,10 +23,18 @@ class SimpleTurtlesimKinematics(Node):
         Tx = self.last_turtle2_pose_.x - self.last_turtle1_pose_.x
         Ty = self.last_turtle2_pose_.y - self.last_turtle1_pose_.y
 
+        theta_rad = self.last_turtle2_pose_.theta - self.last_turtle1_pose_.theta
+        theta_deg = 180 * theta_rad / 3.141592653589793
+
         self.get_logger().info("""\n 
                 Translation vector between Turtle1 -> Turtle2: \n
                 Tx = %f, \n
-                Ty = %f""" % (Tx, Ty))
+                Ty = %f
+                Rotation Matrix turtle1 -> turtle2\n
+                theta(rad) : %f\n
+                theta(degree): %f\n
+                |R11        R12| : |%f      %f|\n
+                |R21        R22| : |%f      %f|""" % (Tx, Ty, theta_rad, theta_deg, math.cos(theta_rad), -math.sin(theta_rad), math.sin(theta_rad), math.cos(theta_rad)))
         
 def main():
     rclpy.init()
