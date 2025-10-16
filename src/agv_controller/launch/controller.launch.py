0509faa98900751f37946c1d34fa6bf88d2976ed
dionisioraditya <dio.prasmada@gmail.com>
2025-10-16 17:45:39 +0700

    UPDATE IMPLEMENTASI DIFFERENTIAL DRIVE CONTROLLER

--- src/agv_controller/launch/controller.launch.py
+++ src/agv_controller/launch/controller.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, GroupAction
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition, UnlessCondition
 
@@ -21,10 +21,16 @@ def generate_launch_description():
         default_value="0.3",
         description="Distance between the wheels in meters"
     )
+    use_simple_controller_arg = DeclareLaunchArgument(
+        "use_simple_controller",
+        default_value="True"
+    )
 
     use_python = LaunchConfiguration("use_python")
     wheel_radius = LaunchConfiguration("wheel_radius")
     wheel_separation = LaunchConfiguration("wheel_separation")
+    use_simple_controller = LaunchConfiguration("use_simple_controller"
+                                                )
 
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
@@ -37,42 +43,61 @@ def generate_launch_description():
         
     )
 
-    simple_controller = Node(
+    wheel_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
         arguments=[
-            'simple_velocity_controller',
+            'agv_controller',
             '--controller-manager',
             '/controller_manager'
+        ],
+        condition=UnlessCondition(use_simple_controller)
+        
+    )
+
+    simple_controller = GroupAction(
+        condition = IfCondition(use_simple_controller),
+        actions=[
+            Node(
+                package='controller_manager',
+                executable='spawner',
+                arguments=[
+                    'simple_velocity_controller',
+                    '--controller-manager',
+                    '/controller_manager'
+                ]
+            ),
+
+            Node(
+                package='agv_controller',
+                executable='simple_controller.py',
+                parameters=[
+                    {'wheel_radius': wheel_radius},
+                    {'wheel_separation': wheel_separation}
+                ],
+                #condition = IfCondition(use_python)
+            )
         ]
     )
 
-    simple_controller_py = Node(
-        package='agv_controller',
-        executable='simple_controller.py',
-        parameters=[
-            {'wheel_radius': wheel_radius},
-            {'wheel_separation': wheel_separation}
-        ],
-        #condition = IfCondition(use_python)
-    )
-    simple_controller_cpp = Node(
-        package='agv_controller',
-        executable='simple_controller',
-        parameters=[
-            {'wheel_radius': wheel_radius},
-            {'wheel_separation': wheel_separation}
-        ],
-        #condition = UnlessCondition(use_python)
-    )
+    
+    # simple_controller_cpp = Node(
+    #     package='agv_controller',
+    #     executable='simple_controller',
+    #     parameters=[
+    #         {'wheel_radius': wheel_radius},
+    #         {'wheel_separation': wheel_separation}
+    #     ],
+    #     #condition = UnlessCondition(use_python)
+    # )
 
     return LaunchDescription([
         use_python_arg,
         wheel_radius_arg,
         wheel_separation_arg,
+        use_simple_controller_arg,
         joint_state_broadcaster_spawner,
-        simple_controller,
-        simple_controller_py,
-        #simple_controller_cpp
+        wheel_controller_spawner,
+        simple_controller
     ])
 
